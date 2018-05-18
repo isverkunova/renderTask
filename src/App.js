6b5f33decfc496b02283128606e09f1816982324
IrenSverkunova <iren.sverkunova@gmail.com>
2018-05-18 17:53:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const tasks = [
+    {
+        id: "xjh",
+        message: "Успешно пройти React-интенсив компании Lectrum",
+        important: true
+    },
+    {
+        id: "xjr",
+        message: "Взять автограф у Джареда Лето",
+        important: true
+    },
+    {
+        id: "xrh",
+        message: "Зарегестрировать бабушку в Твиче",
+        important: false
+    },
+    {
+        id: "rjh",
+        message: "Записать собаку на груминг",
+        important: false
+    },
+    {
+        id: "xph",
+        message: "Научиться играть на барабанах",
+        important: true
+    }
+];
+
+class TaskList extends React.Component {
+    render () {
+        const importantTasks = tasks.filter(task => task.important);
+        
+        const listOfTasks = importantTasks.map(
+            task => <li key = { task.id }>{ task.message }</li>
+        );
+            
+        return <ul>{ listOfTasks }</ul>;
+    }
+};
+
+
+ReactDOM.render(<TaskList />, document.getElementById('app'));
